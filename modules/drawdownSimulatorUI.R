a63842b78682c432841262d56f5a6a3e0ca42d19
Robin Ochieng <robinochieng73@gmail.com>
2024-10-25 21:47:03 +0300

    Updated the drawdownSimulatorUI

--- modules/drawdownSimulatorUI.R
+++ modules/drawdownSimulatorUI.R
@@ -4,10 +4,10 @@ list(
                  actionButton(inputId = "drawdown_resim", label = "Re-Run Simulation", class = "action-button", icon("random")),
                  br(), br(),
                  numericInputIcon(inputId = "drawdown_retire_age", label = "Age at Retirement:", value = 66, min = 55, max = getOmega(ILT15_female_reduced), icon = list(NULL, "Years")),
-                 numericInputIcon(inputId = "drawdown_start_capital", label = "Starting Capital:", value = 300000, min = 0, icon = icon("euro")),
+                 numericInputIcon(inputId = "drawdown_start_capital", label = "Starting Capital:", value = 300000, min = 0, icon = ("KES")),
                  selectInput("drawdown_withdraw_freq", "Withdrawal Frequency:", freq_list_drawdown),
                  awesomeRadio("drawdown_withdraw_type", "Withdrawal Type:", choices = list("Fixed" = F, "Percentage" = T), inline = TRUE),
-                 div(id = "drawdown_annual_withdrawals_input", numericInputIcon(inputId = "drawdown_annual_withdrawals", label = "Total Withdrawals per Annum:", value = 12000, min = 0, icon = icon("euro"))),
+                 div(id = "drawdown_annual_withdrawals_input", numericInputIcon(inputId = "drawdown_annual_withdrawals", label = "Total Withdrawals per Annum:", value = 12000, min = 0, icon = ("KES"))),
                  div(id = "drawdown_percent_withdrawal_input", numericInputIcon(inputId = "drawdown_percent_withdrawal", label = "Percentage Withdrawn per Annum:", value = 4, min = 0, max = 100, icon = list(NULL, icon("percent")))),
                  h5(strong("Portfolio Suggestion Tool:")),
                  actionButton(inputId = "drawdown_surveydisplay", label = "Risk Profiler", class = "action-button", icon("poll")),
@@ -18,7 +18,6 @@ list(
                  numericInputIcon(inputId = "drawdown_annual_inflation", label = "Mean Annual Inflation:", value = 1.5, min = 0, max = 100, icon = list(NULL, icon("percent"))),
                  numericInputIcon(inputId = "drawdown_annual_inf_std_dev", label = "Standard Deviation of Annual Inflation:", value = 1, min = 0, max = 100, icon = list(NULL, icon("percent")))
     ),
-    
     mainPanel(
       uiOutput("drawdown_ui"),
       column(1, actionButton("drawdown_submit", "Next"))
